fab84a3733693d4205ff92b4048f38cd42497b64
wangxingkang <wang_xingkang@qq.com>
2022-08-16 14:59:32 +0800

    docs: 示例优化

--- docs/components/Marker/demos/default.tsx
+++ docs/components/Marker/demos/default.tsx
@@ -1,7 +1,13 @@
 import { LarkMap, Marker } from '@pansy/lark-map';
 
 export default () => (
-  <LarkMap mapType="GaodeV1" style={{ height: 500 }}>
+  <LarkMap
+    mapType="GaodeV1"
+    style={{ height: 500 }}
+    mapOptions={{
+      center: [120.104735, 30.261121],
+    }}
+  >
     <Marker lngLat={{ lng: 120.104735, lat: 30.261121 }} />
     <Marker lngLat={{ lng: 120.210792, lat: 30.246026 }}>
       <div style={{ padding: 8, backgroundColor: 'pink' }}>杭州</div>
