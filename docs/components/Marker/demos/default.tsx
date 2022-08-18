0c8d67c4a084fef501dab961b3866514b2bc1617
wangxingkang <wang_xingkang@qq.com>
2022-08-18 23:07:50 +0800

    docs: update demo

--- docs/components/Marker/demos/default.tsx
+++ docs/components/Marker/demos/default.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { LarkMap, Marker } from '@pansy/lark-map';
 
 export default () => (
@@ -8,7 +9,7 @@ export default () => (
       center: [120.104735, 30.261121],
     }}
   >
-    <Marker lngLat={{ lng: 120.104735, lat: 30.261121 }} />
+    {/* <Marker lngLat={{ lng: 120.104735, lat: 30.261121 }} /> */}
     <Marker lngLat={{ lng: 120.210792, lat: 30.246026 }}>
       <div style={{ padding: 8, backgroundColor: 'pink' }}>杭州</div>
     </Marker>
