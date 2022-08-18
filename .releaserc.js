a6f171232af0cd8164c4db04e835f957bca3666d
wangxingkang <wang_xingkang@qq.com>
2022-08-18 23:14:06 +0800

    chore: add release config

---
+++ .releaserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  branches: ['+([0-9])?(.{+([0-9]),x}).x', 'master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/github",
+    "@semantic-release/npm",
+    "@semantic-release/git",
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          'chore(release): ${nextRelease.gitTag} [skip ci] \n\n${nextRelease.notes}',
+      },
+    ],
+  ]
+}
